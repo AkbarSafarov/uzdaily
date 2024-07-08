ab089b7c8fda7063442d121b871d49502122bcab
Akbar <akbarsafarov.as@gmail.com>
2024-07-08 22:32:35 +0500

    fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $(function(){
+
+    $('.slider_block_wr').each(function(){
+        const slider = $(this).find('.swiper');
+        const sliderId = slider.data('id');
+        const sliderClass = '.' + sliderId;
+        const arrow = slider.data('arrow');    
+
+        const newProductsSwiper = new Swiper(sliderClass, {
+            loop: true,
+            slidesPerView: 1,
+            loopedSlides: 1,
+            navigation: {
+                nextEl: '.swiper-' + arrow + '-next',
+                prevEl: '.swiper-' + arrow + '-prev',
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            effect: "fade",
+            lazy: true
+        });
+    })
 	
 	var $body = $(document.body),
       	$html = $(document.documentElement);
@@ -58,7 +80,7 @@ $(function(){
     });
 
     $(document).on('click', function(e){
-        if( $(e.target).closest('.dropdown-item-current').length || $(e.target).closest(menuBtn).length || $(e.target).closest(menuWrapper).length)
+        if( $(e.target).closest('.search_form_popup').length || $(e.target).closest('.search_btn_mobile').length ||  $(e.target).closest('.dropdown-item-current').length || $(e.target).closest(menuBtn).length || $(e.target).closest(menuWrapper).length)
             return;
         if (menuBtn.hasClass(openedMenu)){
             menuWrapper.removeClass(openedMenu);
@@ -70,6 +92,18 @@ $(function(){
         if ($('.lang_block').hasClass('active')){
             $('.lang_block').removeClass('active')
         }
+
+        if ($('.search_form_popup').hasClass('active')){
+            $('.search_form_popup').removeClass('active')
+        }
+    });
+
+    $('.search_btn_mobile').on('click', function(){
+        if ($('.search_form_popup').hasClass('active')){
+            $('.search_form_popup').removeClass('active');
+        } else {
+            $('.search_form_popup').addClass('active');
+        }
     });
 
     $('.dropdown-item-current').on('click', function(){
@@ -79,6 +113,18 @@ $(function(){
             $(this).parents('.lang_block').addClass('active');
         }
     });
+
+
+    var tmenu = $('.banners_block_wr'),
+        tmenuOffset = tmenu.offset();
+
+    $(window).scroll(function(){
+        if (($(window).scrollTop() > tmenuOffset.top)) {
+            tmenu.addClass('fixed'); 
+        } else {
+            tmenu.removeClass('fixed');
+        };
+    }); 
 });
 
 var backButton = document.querySelector('.back_btn');
@@ -91,49 +137,86 @@ if (backButton) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    const changeElements = document.querySelectorAll('.currencyconverter-minimalistic-change-percentage');
+    function showBannerPopup() {
+        var bannerPopup = document.querySelector('.banner_popup');
+        if(bannerPopup) {
+            if (window.innerWidth <= 1024) {
+                bannerPopup.classList.add('opened');
+                
+                var countdown = 6;
+                var timerText = bannerPopup.querySelector('.timer .text');
+                timerText.textContent = 'Осталось ' + countdown + ' секунд';
 
-    changeElements.forEach(function(element) {
-        let percentage = parseFloat(element.innerText.trim()); // Получаем значение процента и преобразуем его в число
-        if (percentage > 0) {
-            element.classList.add('green');
-        } else {
-            element.classList.add('red');
-        }
-    });
+                var timerInterval = setInterval(function() {
+                    countdown--;
+                    timerText.textContent = 'Осталось ' + countdown + ' секунд';
 
-    const currencyContainers = document.querySelectorAll('.currencyconverter-minimalistic-single-currency');
-
-    currencyContainers.forEach(function(container) {
-        let tickerElement = container.querySelector('.currencyconverter-minimalistic-ticker');
-        let priceElement = container.querySelector('.currencyconverter-minimalistic-currency-price');
-        const price = priceElement.innerText;
-
-        if (tickerElement && priceElement) {
-            let tickerText = tickerElement.innerText.trim();
-            let currencySymbol = '';
-
-            switch (tickerText) {
-                case 'USD':
-                    currencySymbol = '$';
-                    break;
-                case 'EUR':
-                    currencySymbol = '€';
-                    break;
-                case 'RUB':
-                    currencySymbol = '₽';
-                    break;
-                case 'UZS':
-                    currencySymbol = 'UZS';
-                    break;
-                default:
-                    currencySymbol = '';
-                    break;
+                    if (countdown <= 0) {
+                        clearInterval(timerInterval);
+                        bannerPopup.classList.remove('opened');
+                    }
+                }, 1000);
             }
-
-            priceElement.innerText = currencySymbol + '1 – ' + price;
-            tickerElement.innerText = 'UZS – ';
         }
-    });
+    }
+
+    function hideBannerPopup() {
+        var bannerPopup = document.querySelector('.banner_popup');
+        bannerPopup.classList.remove('opened');
+    }
+
+    var closeBannerButton = document.querySelector('.close_banner');
+    closeBannerButton.addEventListener('click', hideBannerPopup);
+
+    showBannerPopup();
 });
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const changeElements = document.querySelectorAll('.currencyconverter-minimalistic-change-percentage');
+
+//     changeElements.forEach(function(element) {
+//         let percentage = parseFloat(element.innerText.trim()); // Получаем значение процента и преобразуем его в число
+//         if (percentage > 0) {
+//             element.classList.add('green');
+//         } else {
+//             element.classList.add('red');
+//         }
+//     });
+
+//     const currencyContainers = document.querySelectorAll('.currencyconverter-minimalistic-single-currency');
+
+//     currencyContainers.forEach(function(container) {
+//         let tickerElement = container.querySelector('.currencyconverter-minimalistic-ticker');
+//         let priceElement = container.querySelector('.currencyconverter-minimalistic-currency-price');
+//         const price = priceElement.innerText;
+
+//         if (tickerElement && priceElement) {
+//             let tickerText = tickerElement.innerText.trim();
+//             let currencySymbol = '';
+
+//             switch (tickerText) {
+//                 case 'USD':
+//                     currencySymbol = '$';
+//                     break;
+//                 case 'EUR':
+//                     currencySymbol = '€';
+//                     break;
+//                 case 'RUB':
+//                     currencySymbol = '₽';
+//                     break;
+//                 case 'UZS':
+//                     currencySymbol = 'UZS';
+//                     break;
+//                 default:
+//                     currencySymbol = '';
+//                     break;
+//             }
+
+//             priceElement.innerText = currencySymbol + '1 – ' + price;
+//             tickerElement.innerText = 'UZS – ';
+//         }
+//     });
+// });
 
